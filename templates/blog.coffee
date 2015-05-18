a355259f4cb31823dcd34197eb535e54dc69f3a4
Varuna Jayasiri <vpjayasiri@gmail.com>
2015-05-18 13:02:27 +0530

    google analytics

--- templates/blog.coffee
+++ templates/blog.coffee
@@ -27,13 +27,13 @@ template = ->
    @link href: "css/paginate.css", rel: "stylesheet"
    @link href: "blog.css", rel: "stylesheet"
    @script '''
-     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-     })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-     ga('create', 'UA-37809145-1', 'auto');
-     ga('send', 'pageview');
+  ga('create', 'UA-44255805-1', 'auto');
+  ga('send', 'pageview');
    '''
 
   @body ->
--- templates/post.js
+++ templates/post.js
@@ -44,7 +44,7 @@
           href: "blog.css",
           rel: "stylesheet"
         });
-        return this.script('(function(i,s,o,g,r,a,m){i[\'GoogleAnalyticsObject\']=r;i[r]=i[r]||function(){\n(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),\nm=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)\n})(window,document,\'script\',\'//www.google-analytics.com/analytics.js\',\'ga\');\n\nga(\'create\', \'UA-37809145-1\', \'auto\');\nga(\'send\', \'pageview\');');
+        return this.script('(function(i,s,o,g,r,a,m){i[\'GoogleAnalyticsObject\']=r;i[r]=i[r]||function(){\n(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),\nm=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)\n})(window,document,\'script\',\'//www.google-analytics.com/analytics.js\',\'ga\');\n\nga(\'create\', \'UA-44255805-1\', \'auto\');\nga(\'send\', \'pageview\');');
       });
       return this.body(function() {
         var file, i, len, ref, results;
